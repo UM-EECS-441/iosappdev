be57135449194bdbe49499ee2d215a7bd7b92a1e
Alan <alantn@umich.edu>
2020-10-30 00:26:58 -0400

    Five nearest pharmacies show up and segue to next screen

--- remedy/remedy/PharmacyVC.swift
+++ remedy/remedy/PharmacyVC.swift
@@ -2,20 +2,25 @@ import Foundation
 import UIKit
 import GoogleMaps
 import GooglePlaces
+import SwiftyJSON
+
+class pharmacy {
+    var name: String
+    var address: String
+    
+  init(name: String, address: String) {
+        self.name = name
+        self.address = address
+    }
+}
 
 class GeoData_1 {
     var lat: Double
     var lon: Double
-    var loc: String
-    var facing: String
-    var speed: String
 
-    init(lat: Double, lon: Double, loc: String, facing: String, speed: String) {
+    init(lat: Double, lon: Double) {
         self.lat = lat
         self.lon = lon
-        self.loc = loc
-        self.facing = facing
-        self.speed = speed
     }
 }
 
@@ -26,7 +31,9 @@ class PharmacyVC: UIViewController, CLLocationManagerDelegate {
     var resultView: UITextView?
     
     let locmanager = CLLocationManager()
-    var geodata = GeoData_1(lat: 0.0, lon: 0.0, loc: "", facing: "", speed: "")
+    var geodata = GeoData_1(lat: 0.0, lon: 0.0)
+    
+    var pharmacies = [pharmacy]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -70,24 +77,7 @@ class PharmacyVC: UIViewController, CLLocationManagerDelegate {
             geodata.lon = location.coordinate.longitude
             print(geodata.lat)
             print(geodata.lon)
-            // Reverse geocode to get user's city name
-            let geocoder = GMSGeocoder()
-            geocoder.reverseGeocodeCoordinate(location.coordinate) { response , error in
-                if let address = response?.firstResult() {
-                    let lines = address.lines! as [String]
-                    if (lines.count > 0) {
-                        // get city name from the first address returned
-                        self.geodata.loc = lines[0].components(separatedBy: ", ")[1]
-                    }
-                }
-            }
-
-            // Get user's speed of movement
-            if (location.speed < 0.0) {
-                // bad reading: probably due to initial fix, try again
-                return
-            }
-            geodata.speed = convertSpeed(location.speed)
+            getPharmList()
             locmanager.stopUpdatingLocation()
         }
     }
@@ -98,32 +88,90 @@ class PharmacyVC: UIViewController, CLLocationManagerDelegate {
             return
         }
         locmanager.stopUpdatingHeading()
-        geodata.facing = convertHeading(newHeading.magneticHeading)
-    }
-    func convertHeading(_ heading: Double) -> String {
-        let compass = ["North", "NE", "East", "SE", "South", "SW", "West", "NW", "North"]
-        let index = Int(round(heading.truncatingRemainder(dividingBy: 360) / 45))
-        return compass[index]
-    }
-    func convertSpeed(_ speed: Double) -> String {
-        switch speed {
-            case 1.2..<5:
-                return "walking"
-            case 5..<7:
-                return "running"
-            case 7..<13:
-                return "cycling"
-            case 13..<30:
-                return "driving"
-            case 30..<56:
-                return "in train"
-            case 56..<256:
-                return "flying"
-            default:
-                return "resting"
-        }
     }
     
+    func getPharmList() {
+        locmanager.startUpdatingLocation()
+        let requestURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=42.274829,-83.729844&rankby=distance&type=drugstore&key=AIzaSyBwVdb3vtPPg_RuaVwaKDlWOLN3woENo6Y"
+
+        var request = URLRequest(url: URL(string: requestURL)!)
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let _ = data, error == nil else {
+                print("NETWORKING ERROR")
+                DispatchQueue.main.async {
+                    self.getPharmList()
+                }
+                return
+            }
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                print("HTTP STATUS: \(httpStatus.statusCode)")
+                DispatchQueue.main.async {
+                    self.getPharmList()
+                }
+                return
+            }
+            
+            do {
+              
+                let json = JSON(try JSONSerialization.jsonObject(with: data!) as! [String:Any])
+//                print(json)
+                
+                var y_val = 200
+                var i = 0;
+                for pharm in json["results"] {
+//                    print(pharm)
+//                    print(pharm.1["name"])
+//                    print(pharm.1["vicinity"])
+//                    print(pharm.1["geometry"]["location"]["lat"])
+//                    print(pharm.1["geometry"]["location"]["lng"])
+//                    print(pharm.1["opening_hours"]["open_now"])
+                    
+                    let name = pharm.1["name"].string
+                    let address = pharm.1["vicinity"].string
+                    let lat = pharm.1["geometry"]["location"]["lat"].double
+                    let lng = pharm.1["geometry"]["location"]["lng"].double
+                    let open = pharm.1["opening_hours"]["open_now"].bool
+                    let image_url = pharm.1["photos"]
+                    
+                    
+                    let btn = UIButton(type: .custom) as UIButton
+                    btn.backgroundColor = UIColor(red: 227/255, green: 120/255, blue: 120/255, alpha: 0.7)
+                    btn.setTitle("\(name!)\n\(address!)", for: .normal)
+                    btn.titleLabel?.textAlignment = NSTextAlignment.center
+                    btn.titleLabel?.lineBreakMode = .byWordWrapping
+                    btn.frame = CGRect(x: 50, y: y_val, width: 320, height: 100)
+                    btn.layer.cornerRadius = 30
+                    btn.addTarget(self, action: #selector(self.clickMe), for: .touchUpInside)
+                    btn.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.2).cgColor
+                    btn.layer.shadowOffset = CGSize(width: 0.0, height: 5.0)
+                    btn.layer.shadowOpacity = 1.0
+                    btn.layer.shadowRadius = 0.4
+                    btn.layer.masksToBounds = false
+                    self.view.addSubview(btn)
+                    
+
+                    y_val = y_val + 120
+                    i = i + 1
+                    if i == 5 {
+                        break
+                    }
+                    
+                }
+            } catch let error as NSError {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+    
+    @objc func clickMe(sender:UIButton!) {
+        print("Button Clicked")
+        performSegue(withIdentifier: "toConfirmation", sender: nil)
+    }
+    
+   
 }
 
 
